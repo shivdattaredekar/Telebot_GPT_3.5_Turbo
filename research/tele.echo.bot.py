261c6f83f65e79fd96bed3ab6bb5eee4547a2edc
Shivdatta redekar <shivdattaredekar@gmail.com>
2024-03-22 08:06:43 +0530

    new update

--- research/tele.echo.bot.py
+++ research/tele.echo.bot.py
@@ -1,12 +1,12 @@
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 import os
 import asyncio
 from os import getenv
 import sys
-
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
 
 load_dotenv()  # This is done to access the .env file
 BOT_TOKEN = os.getenv('TOKEN')
@@ -19,12 +19,13 @@ bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)  # Setting the bot object
 
 # This helps us to connect with the telegram bot
 dp = Dispatcher() 
+
 async def main() -> None:
     # And the run events dispatching
     await dp.start_polling(bot)
 
 # Below is created to start communication with bot
-@dp.message(commands=['start', 'help'])
+@dp.message(CommandStart())
 async def command_start_handler(message: types.Message):
     """
     This handler receives messages with `/start` or `/help `command
@@ -32,5 +33,4 @@ async def command_start_handler(message: types.Message):
     await message.reply("Hi\nI am Echo Bot!\nPowered by aiogram.")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
